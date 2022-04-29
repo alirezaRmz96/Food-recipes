a2b803184e9e5163596e3c6b1f4b87c064ba36d1
Alireza <alirezaramezan200@gmail.com>
2022-04-29 10:15:52 +0430

    first add project

---
+++ app/src/main/java/com/example/food/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+}
